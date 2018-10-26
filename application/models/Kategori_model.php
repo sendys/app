fe9a3d390459bf9bc92f3be2e3182b0d312efc42
SQLHOME <sandy.saryono@gmail.com>
2018-10-26 14:10:38 +0700

    a

--- application/models/Kategori_model.php
+++ application/models/Kategori_model.php
@@ -102,4 +102,94 @@ class Kategori_model extends CI_Model{
 		$this->db->delete($this->table);
 	}
 	
+	 //fungsi simpan
+	 public function screate()
+	 {
+		 $json = array();
+		 $this->form_validation->set_rules('kategori', 'Kategori', 'required');
+		 
+		 if($this->form_validation->run()){
+			 $this->kategori = $this->input->post('kategori');
+			 $res = $this->db->insert('kategori', $this);
+ 
+			 if ($res){
+				 $insert_id = $this->db->insert_id();
+				 $json = array(
+						 'type' => 'success',
+						 'message' =$this->db->get_where('kategori',['id' => $insert_id])->row_array()
+				 );
+			 }else{
+				 $json = array(
+					 'type' => 'error',
+					 'message' => 'sory. data cant be save'
+				 );
+			 }
+		 }else{
+			 $json = array(
+					 'type' =>'error',
+					 'message' => validation_errors()
+			 );
+		 }
+		 header('Content-Type: application/json');
+		 echo json_encode($json);
+	 }
+	 
+	 public function supdate()
+	 {
+		 $json = array();
+		 $this->form_validation->set_rules('id', 'ID', 'required');
+		 $this->form_validation->set_rules('kategori', 'Kategori', 'required');
+ 
+		 if($this->form_validation->run()){
+			 $id	= $this->input->post('id');
+			 $data['kategori']	=$this->input->post('kategori')
+			 $update_id =$this->db->update('kategori', $data,array('id'=>$id));
+			 if ($update_id){
+				 $json = array(
+						 'type' => 'success',
+						 'message' =$this->db->get_where('kategori',['id' => $insert_id])->row_array()
+				 );
+			 }else{
+				 $json = array(
+					 'type' => 'error',
+					 'message' => 'sory. data cant be save'
+				 );
+			 }
+		 }else{
+			 $json = array(
+				 'type' =>'error',
+				 'message' => validation_errors()
+			 );
+		 }
+		 header('Content-Type: application/json');
+		 echo json_encode($json);
+	 }
+ 
+	 public function sdelete()
+	 {
+		 $json = array();
+		 $id = $this->input->post('id');
+ 
+		 if ($id = 0){
+			 $res = $this->db->delete('kategori',['id' => $id]);
+			 if($res != FALSE){
+				 $json = array(
+					 'type' => 'sukses',
+					 'message' => 'Data berhasil di hapus'
+				 );
+			 }else{
+				 $json = array(
+					 'type' => 'sukses',
+					 'message' => 'Data tidak berhasil di hapus'
+				 );
+			 }
+		 }else {
+			 $json = array(
+				 'type' => 'sukses',
+				 'message' => 'Data tidak berhasil di hapus'
+			 );
+		 }
+		 header('Content-Type: application/json');
+		 echo json_encode($json);
+	 }
 }
