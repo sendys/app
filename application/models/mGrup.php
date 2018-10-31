f9a15156d1d1ea17c37064564b04f2a7388a783e
SQLHOME <sandy.saryono@gmail.com>
2018-10-31 10:11:04 +0700

    a

--- application/models/mGrup.php
+++ application/models/mGrup.php
@@ -75,33 +75,16 @@ class mGrup extends CI_Model{
 		$this->db->from($this->table);
 		return $this->db->count_all_results();
 	}
+
 	public function get_by_id($id)
 	{
 		$this->db->from($this->table);
 		$this->db->where('id',$id);
 		$query = $this->db->get();
-
 		return $query->row();
 	}
 	
-	public function save($data)
-	{
-		$this->db->insert($this->table, $data);
-		return $this->db->insert_id();
-	}
 
-	public function update($where, $data)
-	{
-		$this->db->update($this->table, $data, $where);
-		return $this->db->affected_rows();
-	} 
-
-	public function delete_by_id($id)
-	{
-		$this->db->where('id', $id);
-		$this->db->delete($this->table);
-	}
-	
     //fungsi simpan
     public function screate()
     {
